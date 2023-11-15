2999cbea247139849c0d1dfe43a1c41233c27a05
Glicht <rosalesvallorani@gmail.com>
2023-11-15 01:25:05 -0400

    [cambios] - Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 import { Clock } from './layout/Clock';
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
   return (
     <>
       <div className="App">
         <Clock />
+        <Analytics />
       </div>
     </>
   );
